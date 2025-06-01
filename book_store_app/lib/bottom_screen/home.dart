6042a5e16481b299163a4671c17182267241d7b4
preeti <shresthapreeti933@gmail.com>
2025-06-01 22:18:15 +0545

    edege inset change

--- book_store_app/lib/bottom_screen/home.dart
+++ book_store_app/lib/bottom_screen/home.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(15),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
